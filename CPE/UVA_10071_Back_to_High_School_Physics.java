373d51169d42d47bb50c11c17b844f71f8de1630
a4865g <43570004+a4865g@users.noreply.github.com>
2021-10-06 17:42:50 +0800

    2*v*t

---
+++ CPE/UVA_10071_Back_to_High_School_Physics.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class main{
+	public static void main(String[] argv){
+		Scanner sc=new Scanner(System.in);
+		while(sc.hasNext()){
+			int v=sc.nextInt();
+			int t=sc.nextInt();
+			System.out.println(2*v*t);
+		}
+	}
+}
